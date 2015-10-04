4702724761d07793e47f720cd4d5813a63c85836
Stelmashenko-A <stelma_28@bk.ru>
2015-10-04 11:40:22 +0300

    VectorOperand

---
+++ PhysicsCalculator/PhysicsCalculator/VectorOperand.cs
@@ -0,0 +1,67 @@
+﻿namespace PhysicsCalculator
+{
+    class VectorOperand
+    {
+        public Vector2D Value { get; }
+
+        private Measure _measure;
+
+        public Measure MeasurementUnits
+        {
+            get { return _measure.Clone() as Measure; }
+            protected set { _measure = value; }
+        }
+
+        public VectorOperand(Vector2D vector, Measure measure)
+        {
+            Value = vector;
+            _measure = measure;
+        }
+        public static VectorOperand operator +(VectorOperand operand1, VectorOperand operand2)
+        {
+            if (!MeasureCalculator<Measure>.CheckForEquals(operand1.MeasurementUnits, operand2.MeasurementUnits))
+                throw new SumException();
+            return new VectorOperand(operand1.Value + operand2.Value, operand1.MeasurementUnits);
+        }
+
+        public static VectorOperand operator -(VectorOperand operand1, VectorOperand operand2)
+        {
+            if (!MeasureCalculator<Measure>.CheckForEquals(operand1.MeasurementUnits, operand2.MeasurementUnits))
+                throw new SubtractionException();
+            return new VectorOperand(operand1.Value - operand2.Value, operand1.MeasurementUnits);
+        }
+
+        public static VectorOperand operator *(VectorOperand operand1, double operand2)
+        {
+            return new VectorOperand(operand1.Value * operand2, operand1.MeasurementUnits);
+        }
+
+
+        public static VectorOperand operator *(VectorOperand operand1, Operand operand2)
+        {
+            return new VectorOperand(operand1.Value * operand2.Value, new Measure("unnamed",
+                    MeasureCalculator<Measure>.Multiply(operand1.MeasurementUnits.SIequivalent,
+                        operand2.MeasurementUnits.SIequivalent), x => x, x => x));
+        }
+
+        public static VectorOperand operator *(Operand operand1, VectorOperand operand2)
+        {
+            return operand2*operand1;
+        }
+
+        public static VectorOperand operator *(double operand1, VectorOperand operand2)
+        {
+            return new VectorOperand(operand2.Value * operand1, operand2.MeasurementUnits);
+        }
+
+        public static double operator *(VectorOperand operand1, VectorOperand operand2)
+        {
+            return operand1.Value*operand2.Value;
+        }
+
+        public static VectorOperand operator /(VectorOperand operand1, double operand2)
+        {
+            return operand1 * (1 / operand2);
+        }
+    }
+}
